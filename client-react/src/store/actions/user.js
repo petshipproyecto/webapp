ba5db2a1e2db39994a5b21b72c5b23f76a810ad8
Damian Morales <damian_morales_utn@hotmail.com>
2019-10-26 00:36:58 -0300

    removed promise chaining

--- client-react/src/store/actions/user.js
+++ client-react/src/store/actions/user.js
@@ -5,62 +5,58 @@ import config from '../../config'
 var rutaApi = config.rutaApi
 
 export const signIn = (credentials) => {
-    return (dispatch, getState, {getFirebase}) => {
-      const firebase = getFirebase();
-      console.log(getState())
-      
-      firebase.auth().signInWithEmailAndPassword(
-        credentials.email,
-        credentials.password
-      ).then((response) => {
-        console.log( response.user)
-        dispatch({ type: 'LOGIN_SUCCESS', usuario: response.user });
-      }).catch((err) => {
-        dispatch({ type: 'LOGIN_ERROR', err });
-      });
-  
-    }
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    console.log(getState())
+
+    firebase.auth().signInWithEmailAndPassword(
+      credentials.email,
+      credentials.password
+    ).then((response) => {
+      console.log(response.user)
+      dispatch({ type: 'LOGIN_SUCCESS', usuario: response.user });
+    }).catch((err) => {
+      dispatch({ type: 'LOGIN_ERROR', err });
+    });
+
   }
-  
-  export const signOut = () => {
-    return (dispatch, getState, {getFirebase}) => {
-      const firebase = getFirebase();
-  
-      firebase.auth().signOut().then(() => {
-       
-        dispatch({ type: 'SIGNOUT_SUCCESS' })
-      });
-    }
+}
+
+export const signOut = () => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+
+    firebase.auth().signOut().then(() => {
+
+      dispatch({ type: 'SIGNOUT_SUCCESS' })
+    });
   }
-  
-  export const signUp = (newUser) => {
-      
-    return (dispatch, getState, {getFirebase}) => {
-      const firebase = getFirebase();
-  
-      firebase.auth().createUserWithEmailAndPassword(
-        newUser.email, 
-        newUser.password
-      ).then(resp => {
-        // Guarda la ubicación
-        axios.post(rutaApi+'ubicacion',{
-          Descripcion: newUser.ubicacion
-        })
-          .then(newUbicacion => {
-            // Recupera la ubicación y la asigna al nuevo usuario
-            axios.post(rutaApi+'usuario', {           
-                "Usr_cod": resp.user.uid,
-                "Nombre": newUser.firstName,
-                "Apellido": newUser.lastName,
-                "Id_ubicacion": newUbicacion.data.Id_ubicacion
-            }).then(() => {
-              dispatch({ type: 'SIGNUP_SUCCESS' });
-              
-            }).catch((err) => {
-              dispatch({ type: 'SIGNUP_ERROR', err});
-            });
-          })
-      })
+}
+
+export  const signUp = (newUser) => {
+
+  return async (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+
+    const resp = await firebase.auth().createUserWithEmailAndPassword( newUser.email,newUser.password);
+    const newUbicacion = await  axios.post(rutaApi + 'ubicacion', {
+      Descripcion: newUser.ubicacion
+    })
+     // Guarda la ubicación
+     
+          // Recupera la ubicación y la asigna al nuevo usuario
+          axios.post(rutaApi + 'usuario', {
+            "Usr_cod": resp.user.uid,
+            "Nombre": newUser.firstName,
+            "Apellido": newUser.lastName,
+            "Id_ubicacion": newUbicacion.data.Id_ubicacion
+          }).then(() => {
+            dispatch({ type: 'SIGNUP_SUCCESS' });
+
+          }).catch((err) => {
+            dispatch({ type: 'SIGNUP_ERROR', err });
+          });
+        
     }
-  }
   
+}
